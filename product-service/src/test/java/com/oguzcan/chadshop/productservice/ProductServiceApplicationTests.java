bc69135820398e5a23af82a2581dc3545c7eb061
Oguzcan BICER <oguzcanbicer96@gmail.com>
2024-06-21 09:02:31 +0300

    initial commit for Product service and Config Server

---
+++ product-service/src/test/java/com/oguzcan/chadshop/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzcan.chadshop.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
